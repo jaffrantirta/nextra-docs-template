4317c6d7d6c2f597ec07a806904342bab35463ec
jaffrantirta <jaffrantirta@icloud.com>
2024-03-20 14:51:11 +0800

    ready to go

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ldlcuxfczildnousoivx.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pqxdbiaibeqfgxoxlkpg.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "konsultanvisa.com",
+        port: "",
+        pathname: "/static/media/**",
+      },
+    ],
+  },
+});
 
-module.exports = withNextra()
+module.exports = withNextra();
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <Image width={200} height={200} src="/medias/logo.png" alt="" />,
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Konsultan Visa",
   },
-}
+  search: {
+    placeholder: "Search...",
+    loading: true,
+  },
+  feedback: {
+    content: <></>,
+  },
+  editLink: {
+    text: <></>,
+  },
+  darkMode: false,
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - Konsultan Visa",
+    };
+  },
+};
 
-export default config
+export default config;
